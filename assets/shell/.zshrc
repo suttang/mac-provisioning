d305c255b9b0ae84bc46a1b289de6f44585c0997
suttang <suttang@gmail.com>
2022-01-23 00:21:49 +0900

    reset

---
+++ assets/shell/.zshrc
@@ -0,0 +1,85 @@
+export LANG=ja_JP.UTF-8
+export PATH="$HOME/bin:$PATH"
+
+# ファイル名の展開でディレクトリにマッチした場合 末尾に / を付加
+setopt mark_dirs
+# 重複を記録しない
+setopt hist_ignore_dups
+# 重複するコマンドは古い法を削除する
+setopt hist_ignore_all_dups
+# Command correct edition before each completion attempt
+setopt correct
+# Aliased ls needs if file/dir completions work
+setopt complete_aliases
+# 語の途中でもカーソル位置で補完
+setopt complete_in_word
+# ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_slash
+# No remove postfix slash of command line
+setopt noautoremoveslash
+# コマンドラインの引数で --prefix=/usr などの = 以降でも補完できる
+setopt magic_equal_subst
+# タブ・ウィンドウ間で履歴を共有する
+setopt share_history
+# 補完候補一覧でファイルの種別を識別マーク表示 (訳注:ls -F の記号)
+setopt list_types
+# 日本語ファイル名等8ビットを通す
+setopt print_eight_bit
+
+# Alias settings
+alias ll="ls -lG"
+# alias where="command -v"
+# alias su="su -l"
+
+# 履歴ファイルの保存先
+export HISTFILE=${HOME}/.zsh_history
+# メモリに保存される履歴の件数
+export HISTSIZE=10000
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=100000
+
+export PATH="/Users/suttang/.local/bin:$PATH"
+
+# For complement
+autoload -U compinit
+compinit
+## ssh config complement
+function _ssh { compadd `fgrep 'Host ' ~/.ssh/config | awk '{print $2}' | sort`; }
+
+## git
+source ~/.zsh/git-prompt.sh
+fpath=(~/.zsh $fpath)
+zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
+autoload -Uz compinit && compinit
+
+# direnv
+eval "$(direnv hook zsh)"
+
+### Added by Zinit's installer
+if [[ ! -f $HOME/.zinit/bin/zinit.zsh ]]; then
+    print -P "%F{33}▓▒░ %F{220}Installing %F{33}DHARMA%F{220} Initiative Plugin Manager (%F{33}zdharma/zinit%F{220})…%f"
+    command mkdir -p "$HOME/.zinit" && command chmod g-rwX "$HOME/.zinit"
+    command git clone https://github.com/zdharma/zinit "$HOME/.zinit/bin" && \
+        print -P "%F{33}▓▒░ %F{34}Installation successful.%f%b" || \
+        print -P "%F{160}▓▒░ The clone has failed.%f%b"
+fi
+
+source "$HOME/.zinit/bin/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# Load a few important annexes, without Turbo
+# (this is currently required for annexes)
+zinit light-mode for \
+    zinit-zsh/z-a-rust \
+    zinit-zsh/z-a-as-monitor \
+    zinit-zsh/z-a-patch-dl \
+    zinit-zsh/z-a-bin-gem-node
+
+### End of Zinit's installer chunk
+
+zinit light zsh-users/zsh-autosuggestions
+zinit light zdharma/fast-syntax-highlighting
+zinit load zdharma/history-search-multi-word
+zinit ice pick"async.zsh" src"pure.zsh"
+zinit light sindresorhus/pure
---
+++ setup.sh
@@ -0,0 +1,247 @@
+#!/bin/bash -eu
+
+# Setup shell
+ln -s ~/workspace/mac-provisioning/assets/shell/.zshrc ~/.zshrc
+touch ~/.zprofile
+
+# Setup homebrew
+if [ ! -e $(which brew) ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "brew already installed"
+fi
+
+
+#
+# CLI Apps
+# ---------------------------------------------------------
+
+#
+# tmux
+if [ ! -e $(which tmux) ]; then
+    brew install tmux
+    ln -s ~/workspace/mac-provisioning/assets/tmux/.tmux.conf ~/.tmux.conf
+else
+    echo "tmux already installed"
+fi
+
+#
+# mas
+if [ ! -e $(which mas) ]; then
+    brew install mas
+else
+    echo "mas already installed"
+fi
+
+#
+# pyenv
+if [ ! -e $(which pyenv) ]; then
+    {
+        echo ''
+        echo '# pyenv'
+        echo 'export PYENV_ROOT="$HOME/.pyenv"'
+        echo 'export PATH="$PYENV_ROOT/bin:$PATH"'
+        echo 'eval "$(pyenv init --path)"'
+        echo 'eval "$(pyenv virtualenv-init -)"'
+    } >> ~/.zprofile
+    pyenv install 3.8.12
+    pyenv global 3.8.12
+else
+    echo "pyenv already installed"
+fi
+
+#
+# pipenv
+if [ ! -e $(which pipenv) ]; then
+    {
+        echo ''
+        echo '# Pipenv'
+        echo 'export PIPENV_VENV_IN_PROJECT=1'
+    } >> ~/.zprofile
+else
+    echo "pipenv already installed"
+fi
+
+#
+# direnv
+if [ ! -e $(which direnv) ]; then
+    {
+        echo ''
+        echo '# direnv'
+        echo 'eval "$(direnv hook zsh)"'
+    } >> ~/.zprofile
+else
+    echo "direnv already installed"
+fi
+
+#
+# volta
+if [ ! -e $(which volta) ]; then
+    curl https://get.volta.sh | bash
+    {
+        echo ''
+        echo '# NodeJS'
+        echo 'export VOLTA_HOME="$HOME/.volta"'
+        echo 'export PATH="$VOLTA_HOME/bin:$PATH"'
+    } >> ~/.zprofile
+else
+    echo "volta already installed"
+fi
+
+#
+# AWS CLI
+if [ ! -e $(which aws) ]; then
+    brew install awscli
+else
+    echo "aws already installed"
+fi
+
+#
+# GUI Apps
+# ---------------------------------------------------------
+
+#
+# Amazon Music
+if [ ! -e "/Applications/Amazon Music.app" ]; then
+    brew install --cask amazon-music
+else
+    echo "Amazon Music already installed"
+fi
+
+#
+## Authy
+if [ ! -e "/Applications/Authy.app" ]; then
+    brew install --cask authy
+else
+    echo "authy already installed"
+fi
+
+#
+# Bitwarden
+if [ ! -e "/Applications/Bitwarden.app" ]; then
+    brew install --cask bitwarden
+    brew install bitwarden-cli
+    bw login
+else
+    echo "bitwarden already installed"
+fi
+
+#
+# Docker
+if [ ! -e "/Applications/Docker.app" ]; then
+    brew install --cask docker
+else
+    echo "docker already installed"
+fi
+
+#
+# Google Chrome
+if [ ! -e "/Applications/Google Chrome.app" ]; then
+    brew install --cask google-chrome
+else
+    echo "Google Chrome already installed"
+fi
+
+#
+# Google 日本語入力
+if [ ! -e "/Applications/GoogleJapaneseInput.localized" ]; then
+    brew install --cask google-japanese-ime
+else
+    echo "Google日本語入力 already installed"
+fi
+
+#
+## Hyper
+if [ ! -e "/Applications/Hyper.app" ]; then
+    brew install --cask hyper
+    ln -s ~/workspace/mac-provisioning/assets/hyper/.hyper.js ~/.hyper.js
+else
+    echo "Raycast already installed"
+fi
+
+#
+# Karabiner-elements
+if [ ! -e "/Applications/Karabiner-Elements.app" ]; then
+    brew install --cask karabiner-elements
+else
+    echo "karabiner-elements already installed"
+fi
+
+#
+# Keka
+if [ ! -e "/Applications/Keka.app" ]; then
+    brew install --cask keka
+else
+    echo "keka already installed"
+fi
+
+#
+# Lunar
+if [ ! -e "/Applications/Lunar.app" ]; then
+    brew install --cask lunar
+else
+    echo "lunar already installed"
+fi
+
+#
+# Magnet
+if [ ! -e "/Applications/Magnet.app" ]; then
+    mas install 441258766
+else
+    echo "magnet already installed"
+fi
+
+#
+# Netron
+if [ ! -e "/Applications/Netron.app" ]; then
+    brew install --cask netron
+else
+    echo "Netron already installed"
+fi
+
+#
+# Obsidian
+if [ ! -e "/Applications/Obsidian.app" ]; then
+    brew install --cask obsidian
+else
+    echo "Obsidian already installed"
+fi
+
+#
+## Raycast
+if [ ! -e "/Applications/Raycast.app" ]; then
+    brew install --cask raycast
+    open "raycast://extensions/pomdtr/bitwarden?source=webstore"
+    open "raycast://extensions/guga4ka/authy?source=webstore"
+else
+    echo "Raycast already installed"
+fi
+
+#
+## Slack
+if [ ! -e "/Applications/Slack.app" ]; then
+    brew install --cask slack
+else
+    echo "Slack already installed"
+fi
+
+#
+# VSCode
+if [ ! -e "/Applications/Visual Studio Code.app" ]; then
+    brew install --cask visual-studio-code
+else
+    echo "Visual Studio Code already installed"
+fi
+
+#
+# Zoom
+if [ ! -e "/Applications/zoom.us.app" ]; then
+    brew install --cask zoom
+else
+    echo "Zoom Code already installed"
+fi
+
+
+## The Unarchiver
+brew install --cask the-unarchiver
+
